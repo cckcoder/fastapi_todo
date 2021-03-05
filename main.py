29241232bd751ae2f6781304294bf2ce4902d1a0
codewiz <c.chaiwat.ch@gmail.com>
2021-03-05 22:03:59 +0700

    update: pydantic example

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+
+
+class Coffee(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    star: int
+
+dummy_db = [
+    {
+        'name': 'Espresso',
+        'description': 'เป็นกาแฟดำที่เข้มที่สุด เป็นจุดเริ่มต้นของกาแฟทุกแก้ว จะถูกเสิร์ฟในแก้วกาแฟร้อนที่เล็กๆ',
+        'price': 60,
+        'star': 5
+    },
+    {
+        'name': 'Americano',
+        'description': 'เป็นการเทกาแฟ Espresso 1 ช็อต  ตามด้วยน้ำร้อน หรือน้ำธรรมดา',
+        'price': 55,
+        'star': 5
+    }
+]
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def hello_world():
-    return {'hello': 'world'}
+def show_all_coffee():
+    return dummy_db
 
 
-@app.get("/items/me")
-def read_item_me():
-    return {'item_id': 'current item'}
+@app.get("/coffee/{coffee_id}")
+def get_items_by_id(coffee_id: int):
+    coffee = dummy_db[coffee_id - 1]
+    return coffee
 
 
-@app.get("/items/{item_id}")
-def get_items_by_id(item_id: int, q: Optional[str] = None):
-    if q:
-        return {'item_id': item_id, 'q': q}
-    return {'item_id': f'Hello item no {item_id}'}
+@app.post("/coffee")
+def create_coffee(coffee: Coffee):
+    dummy_db.append(coffee)
+    return dummy_db[-1]
+
+
+@app.delete("/coffee/{coffee_id}")
+def delete_coffee(coffee_id: int):
+    coffee = dummy_db[coffee_id - 1]
+    dummy_db.pop(coffee_id - 1)
+    result = {'msg': f"{coffee['name']} was delete!"}
+    return result
+
